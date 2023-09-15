6357e2ccde242fff0756a829d76747bf97d10eef
Khushi Gupta <shubhigupta988@gmail.com>
2023-09-15 14:00:43 +0530

    first commit

---
+++ Basic/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolore laborum aperiam cum pariatur neque ad, recusandae possimus consequuntur assumenda tenetur, rem molestiae fugit. Voluptatem praesentium incidunt sunt dolorem voluptate dignissimos perspiciatis numquam unde error iure. Dolorum esse ipsum ducimus sit porro qui. Ex, iste nihil.</h1>
+    </>
+  )
+}
+
+export default App
